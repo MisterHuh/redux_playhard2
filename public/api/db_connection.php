53c45901689e0eb63886cb4919063ab67a8e3055
MisterHuh <jaehuh86@yahoo.com>
2020-07-18 18:38:15 -0700

    added db files

--- public/api/db_connection.php
+++ public/api/db_connection.php
@@ -1,11 +1,3 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', 'root', 'show_me_the_money');
-
-if (!$conn) {
-  throw new Exception(mysqli_connect_error());
-};
-
-if (!$conn->set_charset("utf8")) {
-  printf("Error loading character set utf8: %s\n", $conn->error);
-}
+echo ("Hello World");
