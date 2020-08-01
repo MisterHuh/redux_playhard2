5d894bf7fa3fcdb068d1e01ff7979b9085160c41
MisterHuh <jaehuh86@yahoo.com>
2020-08-01 10:36:21 -0700

    successfully connected to the store. rendering data from the redux store

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -2,8 +2,8 @@ import { FETCH_POSTS } from './types';
 
 export function fetchPosts() {
   return function (dispatch) {
-    console.log("fetching");
-    fetch('jsonplaceholder.typicode.com/posts')
+    console.log("reducing");
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
       .then(items => dispatch({
         type: FETCH_POSTS,
